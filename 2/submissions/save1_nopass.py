94d6a56d93054f481422c11ed82b355ce61cfe37
JnyJny <erik.oshaughnessy@gmail.com>
2022-01-22 15:01:41 -0600

    'submission Bite 2 @ codechalleng.es'

---
+++ 2/submissions/save1_nopass.py
@@ -0,0 +1,27 @@
+import re
+
+def extract_course_times():
+    """Write a regular expression that returns a list of timestamps:
+        ['01:47', '32:03', '41:51', '27:48', '05:02']"""
+    flask_course = ('Introduction 1 Lecture 01:47'
+                    'The Basics 4 Lectures 32:03'
+                    'Getting Technical!  4 Lectures 41:51'
+                    'Challenge 2 Lectures 27:48'
+                    'Afterword 1 Lecture 05:02')
+    return [re.findall('[0-9][0-9]:[0-9][0-9]', line) for line in flask_course]
+
+
+def get_all_hashtags_and_links():
+    """Write a regular expression that returns this list:
+       ['http://pybit.es/requests-cache.html', '#python', '#APIs']"""
+    tweet = ('New PyBites article: Module of the Week - Requests-cache '
+             'for Repeated API Calls - http://pybit.es/requests-cache.html '
+             '#python #APIs')
+    pass
+
+
+def match_first_paragraph():
+    """Write a regular expression that returns  'pybites != greedy' """
+    html = ('<p>pybites != greedy</p>'
+            '<p>not the same can be said REgarding ...</p>')
+    pass
